6eb9d5098396cdd9d127ba931e80e31242e4f175
Ceramicskate0 <ceramicskate0@users.noreply.github.com>
2019-06-02 14:36:19 -0400

    Update Webclient.cs

--- Webclient.cs
+++ Webclient.cs
@@ -12,12 +12,14 @@ namespace dotNetWebClient
           //pass args to file to download file else dont if hardcoded
           if (args.Length>0)
           {
+              //Download data will load to memory
              //Wclient.DownloadData(args[0], args[1])
              Wclient.DownloadFile(args[0], args[1]);
              //Put your code to deal with file here
           }
           else
           {
+              //Download data will load to memory
              //Wclient.DownloadData(//PUT YOUR CODE HERE, //PUT YOUR CODE HERE);
              Wclient.DownloadFile(//PUT YOUR CODE HERE, //PUT YOUR CODE HERE);
              //Put your code to deal with file here
